0d059139ed21412f64ffe483ecb6d93bdcd9332c
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2023-02-08 12:17:26 +0330

    change design - add slider - control selectedOption

--- src/App.js
+++ src/App.js
@@ -1,21 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 
 // image & style
 import s from "./App.module.css";
 import imgSource from "./assets/image/testImage.jpg";
 
+// Components
+import Slider from "./Slider";
+
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState("Brightness");
+  const optionClick = (e) => {
+    setSelectedOption(e.target.name);
+  };
   return (
     <div className={s.container}>
       <img src={imgSource} />
       <div className={s.optionContainer}>
-        <button>Brigtness</button>
-        <button>Contrast</button>
-        <button>Saturation</button>
-        <button>Sepia</button>
-        <button>Gray Scale</button>
-        <button>Hue Rotate</button>
-        <button>Blur</button>
+        <button
+          name="Brightness"
+          onClick={optionClick}
+          className={
+            selectedOption === "Brightness"
+              ? `${s.optionItem} ${s.activeButton}`
+              : s.optionItem
+          }
+        >
+          Brightness
+        </button>
+        <button
+          name="Contrast"
+          onClick={optionClick}
+          className={
+            selectedOption === "Contrast"
+              ? `${s.optionItem} ${s.activeButton}`
+              : s.optionItem
+          }
+        >
+          Contrast
+        </button>
+        <button
+          name="Saturation"
+          onClick={optionClick}
+          className={
+            selectedOption === "Saturation"
+              ? `${s.optionItem} ${s.activeButton}`
+              : s.optionItem
+          }
+        >
+          Saturation
+        </button>
+        <button
+          name="Sepia"
+          onClick={optionClick}
+          className={
+            selectedOption === "Sepia"
+              ? `${s.optionItem} ${s.activeButton}`
+              : s.optionItem
+          }
+        >
+          Sepia
+        </button>
+        <button
+          name="Gray Scale"
+          onClick={optionClick}
+          className={
+            selectedOption === "Gray Scale"
+              ? `${s.optionItem} ${s.activeButton}`
+              : s.optionItem
+          }
+        >
+          Gray Scale
+        </button>
+        <button
+          name="Hue Rotate"
+          onClick={optionClick}
+          className={
+            selectedOption === "Hue Rotate"
+              ? `${s.optionItem} ${s.activeButton}`
+              : s.optionItem
+          }
+        >
+          Hue Rotate
+        </button>
+        <button
+          name="Blur"
+          onClick={optionClick}
+          className={
+            selectedOption === "Blur"
+              ? `${s.optionItem} ${s.activeButton}`
+              : s.optionItem
+          }
+        >
+          Blur
+        </button>
+        <Slider />
       </div>
     </div>
   );
---
+++ src/Slider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Slider = () => {
+  return (
+    <div>
+      <input type="range" />
+    </div>
+  );
+};
+
+export default Slider;
