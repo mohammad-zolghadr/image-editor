d22cc279ffd9808410cc2209b4a1d3b1975235ab
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2023-02-08 11:25:46 +0330

    Some Design -> image & buttons and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+// image & style
+import s from "./App.module.css";
+import imgSource from "./assets/image/testImage.jpg";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div className={s.container}>
+      <img src={imgSource} />
+      <div className={s.optionContainer}>
+        <button>Brigtness</button>
+        <button>Contrast</button>
+        <button>Saturation</button>
+        <button>Sepia</button>
+        <button>Gray Scale</button>
+        <button>Hue Rotate</button>
+        <button>Blur</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import style from "./index.module.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
